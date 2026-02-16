45c3f88f9c69b35d56a7f0431f06732dfd1f980f
DwifteJB <dwifte@outlook.com>
2026-02-16 00:40:46 +0000

    fix: package names

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.hindsightchat"
+    namespace = "me.rmfosho.hindsightchat"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
--- android/app/src/main/kotlin/me/rmfosho/hindsightchat/MainActivity.kt
+++ android/app/src/main/kotlin/me/rmfosho/hindsightchat/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hindsightchat
+package me.rmfosho.hindsightchat
 
 import io.flutter.embedding.android.FlutterActivity
 
